499cca21fa0ead5877ac14d0dfe32d8732add061
xuejinzhong <774070829@qq.com>
2018-07-17 15:18:03 +0800

    添加了后台模版

---
+++ src/main/java/top/xjrlife/dynamic/controller/UserController.java
@@ -0,0 +1,15 @@
+package top.xjrlife.dynamic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 创建人：xjz
+ * 创建时间：2018/7/17
+ */
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    
+}
